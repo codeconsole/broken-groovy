472f37e7c3860a67dd1692b46ab92c5c98d77c04
Scott Murphy <scott@alwaysvip.com>
2012-10-04 18:51:22 -0700

    Display error in html.

--- src/main/webapp/WEB-INF/groovy/test.groovy
+++ src/main/webapp/WEB-INF/groovy/test.groovy
@@ -1,7 +1,22 @@
 import groovy.json.JsonBuilder
 
-response.contentType = 'application/json'
 def output = new JsonBuilder()
 def map = [ fullName: 'Test User' ]
 output.user map
-println output.toString()
+response.contentType = "text/html"
+print """\
+<html>
+<head><title>Groovy/App Engine Bug</title>
+<body>"""
+try {
+	print "<h1>Error Free :)</h1><pre>${output.toString()}</pre>"	
+} catch (Throwable t) {
+	def stacktrace = new StringWriter()
+	def errWriter = new PrintWriter(stacktrace)
+    t.printStackTrace(errWriter)
+	print "<h1>${t.toString()}</h1><pre>${stacktrace}</pre>"
+}
+print """\
+</body>
+</html>
+"""
